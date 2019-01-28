63757981d6688c9ded8361cc5b01a3e33c4846b5
KristofKoblos <kristofkoblos@gmail.com>
2019-01-28 08:21:22 +0100

    Add files via upload

--- post.php
+++ post.php
@@ -1,3 +1,5 @@
 <?php
+  header('Access-Control-Allow-Origin: *');;
+  header('Access-Control-Allow-Method: GET, POST, PATCH, PUT, DELETE, OPTIONS');;
   echo json_encode(array("name"=>"John", "age"=>"18", "gender"=>"Male"));
 ?>
